5409bb13d66bc7be910881ae602bf0d86489a38f
njek02 <n.schepers@student.rug.nl>
2026-01-09 11:43:35 +0100

    Fixed evaluate_model

--- eval/evaluate_model.py
+++ eval/evaluate_model.py
@@ -5,28 +5,34 @@ import numpy as np
 
 def evaluate_final_model(model, make_env_fn, episodes=20, horizon=200, runs=10):
     success_per_run = []
+
     for run in range(runs):
         successes = 0
         rewards = []
+
         for ep in range(episodes):
             env = make_env_fn()
             obs, _ = env.reset()
-            ep_reward = 0
+
+            ep_reward = 0.0
             ep_success = False
 
-        for _ in range(horizon):
-            action, _ = model.predict(obs, deterministic=True)
-            obs, reward, done, truncated, info = env.step(action)
-            ep_reward += reward
+            for _ in range(horizon):
+                action, _ = model.predict(obs, deterministic=True)
+                obs, reward, done, truncated, info = env.step(action)
 
-            if info.get("success", 0) == 1:
-                ep_success = True
+                ep_reward += reward
 
-            if done or truncated:
-                break
+                if info.get("success", 0) == 1:
+                    ep_success = True
+
+                if done or truncated:
+                    break
 
             rewards.append(ep_reward)
             successes += int(ep_success)
+
+            env.close()
 
         success_rate = successes / episodes
         avg_reward = np.mean(rewards)
@@ -37,3 +43,4 @@ def evaluate_final_model(model, make_env_fn, episodes=20, horizon=200, runs=10):
         print(f"Run {run}: Average reward: {avg_reward:.2f}")
 
     plot_success_rate_se(success_per_run, True)
+
