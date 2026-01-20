f85b7363f6ab45b9e5cd8269c955274eece43f15
njek02 <n.schepers@student.rug.nl>
2026-01-20 19:31:39 +0100

    Added efficiency tracker

--- eval/evaluate_model.py
+++ eval/evaluate_model.py
@@ -5,10 +5,12 @@ import numpy as np
 
 def evaluate_final_model(model, make_env_fn, episodes=20, horizon=200, runs=10):
     success_per_run = []
+    efficiency_per_run = []
 
     for run in range(runs):
         successes = 0
         rewards = []
+        steps_to_success = []
 
         for ep in range(episodes):
             env = make_env_fn()
@@ -17,7 +19,7 @@ def evaluate_final_model(model, make_env_fn, episodes=20, horizon=200, runs=10):
             ep_reward = 0.0
             ep_success = False
 
-            for _ in range(horizon):
+            for num_step in range(horizon):
                 action, _ = model.predict(obs, deterministic=True)
                 obs, reward, done, truncated, info = env.step(action)
 
@@ -25,6 +27,8 @@ def evaluate_final_model(model, make_env_fn, episodes=20, horizon=200, runs=10):
 
                 if info.get("success", 0) == 1:
                     ep_success = True
+                    steps_to_success.append(num_step)
+                    break
 
                 if done or truncated:
                     break
@@ -36,11 +40,16 @@ def evaluate_final_model(model, make_env_fn, episodes=20, horizon=200, runs=10):
 
         success_rate = successes / episodes
         avg_reward = np.mean(rewards)
+        avg_efficiency = np.mean(steps_to_success)
 
         success_per_run.append(success_rate)
+        efficiency_per_run.append(avg_efficiency)
 
-        print(f"Run {run}: Success rate: {success_rate:.2f}")
-        print(f"Run {run}: Average reward: {avg_reward:.2f}")
+        print(f"Run {run + 1}: Success rate: {success_rate:.2f}")
+        print(f"Run {run + 1}: Average reward: {avg_reward:.2f}")
+        print(f"Run {run + 1}: Average efficiency: {avg_efficiency:.2f} steps")
+    
+    return success_per_run, efficiency_per_run
 
-    plot_success_rate_se(success_per_run, True)
+    # plot_success_rate_se(success_per_run, True)
 
